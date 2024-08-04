6c83d6671e9a09b0b5f41e6caf928336d93db01c
Sanjana <sanjanaaa.maiya@gmail.com>
2024-08-04 19:55:17 +0530

    Added 3 more functionalities : TitleCase,ClipboardCopy,ClearText

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -4,19 +4,50 @@ import React from 'react'
 
 export default function TextForm(props) {
 
+
+  function clipboardCopy() {
+    // Get the text field
+    var copyText = text;
+    navigator.clipboard.writeText(copyText);
+  
+  }
+
+
   const handleUpClick=() => {
     // console.log("Uppercase converter button was clicked");
     let newText=text.toUpperCase();
     setText(newText);
   }
 
+
+  const handleLoClick=()=>{
+    let newText=text.toLowerCase();
+    setText(newText);
+  }
+
+
+  const handleClearClick=()=>{
+    let newText='';
+    setText(newText);
+  }
+  
+
+  const handleTcClick=()=>{
+    let newText=text.replace(
+      /\w\S*/g,
+      text => text.charAt(0).toUpperCase() + text.substring(1).toLowerCase()
+    );;
+    setText(newText);
+  }
+  
+
   const handleOnChange=(event) => {
     // console.log("Textarea was changed");
     setText(event.target.value);
   }
   
-  
-  const [text, setText] = useState('Enter text here');
+
+  const [text, setText] = useState('');
   
   return (
     <>
@@ -25,12 +56,21 @@ export default function TextForm(props) {
           <div className="mb-3">
           <label htmlFor="myBox" className="form-label"></label>
           <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="14"></textarea>
-          <button className="btn btn-primary my-3" onClick={handleUpClick}>Convert to Uppercase</button>
+          <button className="btn btn-primary my-3 mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
+          <button className="btn btn-primary my-3 mx-1" onClick={handleLoClick}>Convert to Lowercase</button>
+          <button className="btn btn-primary my-3 mx-1" onClick={handleTcClick}>Convert to TitleCase</button>
+          <button className="btn btn-primary my-3 mx-1" onClick={clipboardCopy}>Copy to Clipboard</button>
+          <button className="btn btn-primary my-3 mx-1" onClick={handleClearClick}>Clear Text</button>
+
+
           </div>
       </div>
       <div className="container">
-        <h1>Your text summary</h1>
+        <h2>Your text summary</h2>
         <p>{text.split(" ").length} words,{text.length} characters</p>
+        <p>{0.008 * text.split(" ").length} minutes required to read the text </p>
+        <h2>Preview</h2>
+        <p>{text}</p>
       </div>
     </>
   )
