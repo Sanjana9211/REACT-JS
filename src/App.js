52395cb3c94b8451a8518e8f48797c877162191f
Sanjana <sanjanaaa.maiya@gmail.com>
2024-08-05 22:21:54 +0530

    Alert component added

--- src/App.js
+++ src/App.js
@@ -3,27 +3,43 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React from 'react'
+import Alert from './components/Alert';
 
 
 function App() {
 
   const [mode,setMode]=useState('light');
 
+  const [alert,setAlert]=useState(null);
+
+  const showAlert=(message,type)=>{
+    setAlert({
+      msg : message,
+      type : type
+    });
+  }
+
   const toggleMode=()=>{
     if(mode==='light'){
       setMode('dark');
       document.body.style.backgroundColor='#051d34';
+      showAlert('Dark mode has been enabled',"sucess");
     }
     else{
       setMode('light');
       document.body.style.backgroundColor='white';
-
+      showAlert('Dark mode has been enabled',"sucess");
     }
   }
 
   return (
     <>
     <Navbar title="TextUtils" aboutText='About' mode={mode} toggleMode={toggleMode}/>
+
+
+    <Alert alert={alert}/>
+
+
     <div className="container my-3">
       <TextForm heading="Enter the text to analyze" mode={mode}/>
       {/* <About/> */}
